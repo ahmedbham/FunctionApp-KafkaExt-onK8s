3d05864229ff58dea6e83496800577f1e0ca96af
Ahmed Bham <ahmed.bham@gmail.com>
2021-10-06 20:10:02 +0000

    removed dapr projects. added az-func-kafka-ext

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -2,64 +2,9 @@
 
 echo "on-create start" >> ~/status
 
-# # prevent the dependency popup
-# dotnet restore src/csharp/weather.csproj
-
-# # create a network
-docker network create dapr
-
 # # create local registry
 k3d registry create registry.localhost --port 5000
 docker network connect dapr k3d-registry.localhost
-
-# create dapr-springboot consumer app
-cd dapr-springboot/demo-dapr-consumer
-./mvnw clean install -DskipTests
-./mvnw spring-boot:build-image -DskipTests
-docker tag demo-dapr-consumer:0.0.1-SNAPSHOT k3d-registry.localhost:5000/dapr-consumer
-docker push k3d-registry.localhost:5000/dapr-consumer
-docker rmi demo-dapr-consumer:0.0.1-SNAPSHOT
-cd ../../
-
-# create dapr-springboot producer app
-cd dapr-springboot/demo-dapr-producer
-./mvnw clean install -DskipTests
-./mvnw spring-boot:build-image -DskipTests
-docker tag demo-dapr-producer:0.0.1-SNAPSHOT k3d-registry.localhost:5000/dapr-producer
-docker push k3d-registry.localhost:5000/dapr-producer
-docker rmi demo-dapr-producer:0.0.1-SNAPSHOT
-cd ../../
-
-
-# create springboot-k8s consumer application
-cd springboot-k8s/demo-consumer
-./mvnw clean install -DskipTests
-./mvnw spring-boot:build-image -DskipTests
-docker tag demo-consumer:0.0.1-SNAPSHOT k3d-registry.localhost:5000/springboot-consumer
-docker push k3d-registry.localhost:5000/springboot-consumer
-docker rmi demo-consumer:0.0.1-SNAPSHOT
-cd ../../
-
-
-# create springboot-k8s producer application
-cd springboot-k8s/demo-producer
-./mvnw clean install -DskipTests
-./mvnw spring-boot:build-image -DskipTests
-docker tag demo:0.0.1-SNAPSHOT k3d-registry.localhost:5000/springboot-producer
-docker push k3d-registry.localhost:5000/springboot-producer
-docker rmi demo:0.0.1-SNAPSHOT
-cd ../../
-
-# create dapr subsub producer
-cd dapr-springboot/pubsub-dapr-producer/producer
-./mvnw clean install
-./mvnw spring-boot:build-image
-docker tag producer:0.0.1-SNAPSHOT k3d-registry.localhost:5000/pubsub-producer
-docker push k3d-registry.localhost:5000/pubsub-producer
-docker rmi producer:0.0.1-SNAPSHOT
-cd ../../../
-
-
 
 # create kafka-function
 cd azure-functions-kafka-extension/kafka-function
